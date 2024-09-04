f7f4f7614f8132e74e83955cc4ac3dcf903fdc86
Yahya Mohamed <yahyaimohamed2000@gmail.com>
2024-09-04 14:02:07 +0100

    updated titles

--- src/app/(pages)/aboutus/page.tsx
+++ src/app/(pages)/aboutus/page.tsx
@@ -12,18 +12,18 @@ const AboutUs = () => {
       <Header />
       <HeaderBackground text="About Us" />
       <Paragraph
-        title="test"
+        title="Our Story"
         text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et eum fugiat quo voluptas nulla pariatur?"
         image="https://via.placeholder.com/600x400"
       />
       <Paragraph
-        title="test"
+        title="Our Purpose"
         text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et eum fugiat quo voluptas nulla pariatur?"
         image="https://via.placeholder.com/600x400"
         isReversed={true}
       />
       <Paragraph
-        title="test"
+        title="Our Work"
         text="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et eum fugiat quo voluptas nulla pariatur?"
         isCentered={true}
       />
