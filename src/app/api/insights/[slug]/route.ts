e1234c8e7c5178f7cf11c7c556f8066d537f9fc5
Yahya Mohamed <yahyaimohamed2000@gmail.com>
2024-09-08 19:10:13 +0100

    AHHHH

--- src/app/api/insights/[slug]/route.ts
+++ src/app/api/insights/[slug]/route.ts
@@ -2,7 +2,10 @@
 import { NextResponse } from "next/server";
 import { fetchEntryBySlug } from "@/lib/contentful";
 
-export async function GET({ params }: { params: { slug: string } }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: { slug: string } }
+) {
   try {
     const entry = await fetchEntryBySlug("article", params.slug); // Using slug
     return NextResponse.json(entry);
