50d4523151f51beff68b1e9be660ffdd19016174
Yahya Mohamed <yahyaimohamed2000@gmail.com>
2024-09-12 09:44:56 +0100

    trying to fix up a bug

---
+++ src/app/lib/constants.ts
@@ -0,0 +1,66 @@
+export const navigationData = [
+  { href: "/about", label: "About us" },
+  { href: "/insights", label: "Insights" },
+  { href: "/projects", label: "Projects" },
+  { href: "/services", label: "Services" },
+  { href: "/contact", label: "Contact" },
+];
+
+export const footerData = [
+  {
+    heading: "ABOUT US",
+    links: [
+      { title: "Our Mission", url: "/mission" },
+      { title: "Our Impact", url: "/impact" },
+      { title: "Leadership and Governance", url: "/leadership" },
+      { title: "Forum Statements", url: "/statements" },
+      { title: "Partners", url: "/partners" },
+      { title: "Sustainability at the Forum", url: "/sustainability" },
+      { title: "History", url: "/history" },
+      { title: "Careers", url: "/careers" },
+      { title: "Contact Us", url: "/contact" },
+    ],
+  },
+  {
+    heading: "EVENTS",
+    links: [
+      { title: "Events", url: "/events" },
+      { title: "Open Forum", url: "/open-forum" },
+    ],
+  },
+
+  {
+    heading: "MORE FROM THE GROUP",
+    links: [
+      { title: "Strategic Intelligence", url: "/strategic-intelligence" },
+      { title: "UpLink", url: "/uplink" },
+      { title: "Global Shapers", url: "/global-shapers" },
+      { title: "Young Global Leaders", url: "/young-global-leaders" },
+      { title: "Centre for the Fourth Industrial Revolution", url: "/cfir" },
+    ],
+  },
+  {
+    heading: "MEDIA",
+    links: [
+      { title: "Press", url: "/press" },
+      { title: "Subscribe to our press releases", url: "/press-releases" },
+      { title: "Pictures", url: "/pictures" },
+    ],
+  },
+  {
+    heading: "PARTNERS & MEMBERS",
+    links: [
+      { title: "Sign in", url: "/signin" },
+      { title: "Join Us", url: "/join" },
+    ],
+  },
+  {
+    heading: "LANGUAGE EDITIONS",
+    links: [
+      { title: "English", url: "/en" },
+      { title: "Español", url: "/es" },
+      { title: "中文", url: "/cn" },
+      { title: "日本語", url: "/jp" },
+    ],
+  },
+];
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Layout>
       <main className="hide-scrollbar ">
-        <Header noAnimations={false} />
+        <Header isDark={true} />
         <HeroImage
           title={"Placeholder Featured Insight #1 Technology"}
           backgroundImage={bgImage}
